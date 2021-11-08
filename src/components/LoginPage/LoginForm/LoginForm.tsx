b7c01c5335ccbd059dd8226a4567c948f55fd8f4
Turkin Danila <nil.ottoman@gmail.com>
2021-11-08 21:47:26 +0400

    V-07_F-08 refactored after review

--- src/components/LoginPage/LoginForm/LoginForm.tsx
+++ src/components/LoginPage/LoginForm/LoginForm.tsx
@@ -1,5 +1,7 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
 import { useForm } from 'react-hook-form';
 import { useAppDispatch, useAppSelector } from '../../../utils/hooks';
 import {
@@ -11,16 +13,27 @@ import Loader from '../../ui/Loader/Loader';
 import { ILoginData } from '../../../utils/types';
 import styles from './LoginForm.module.scss';
 
-
 const LoginForm = () => {
   const dispatch = useAppDispatch();
   const isError = useAppSelector(selectIsLoginError);
   const isSubmitting = useAppSelector(selectIsSubmitting);
 
+  const schema = yup.object({
+    username: yup
+      .string()
+      .max(15, 'Не более 15 символов')
+      .required('Это поле обязательно к заполнению'),
+    password: yup
+      .string()
+      .max(15, 'Не более 15 символов')
+      .required('Это поле обязательно к заполнению'),
+  });
+
   const {
     register,
     handleSubmit,
-  } = useForm<ILoginData>();
+    formState: { errors },
+  } = useForm<ILoginData>({ resolver: yupResolver(schema) });
 
   const onSubmit = handleSubmit((data) => dispatch(login(data)));
 
@@ -32,37 +45,49 @@ const LoginForm = () => {
         <form className={styles.form} onSubmit={onSubmit}>
           <div className={styles.title}>Вход</div>
 
-          {isError ? (
-            <div className={styles.formErrorMessage}>
-              Неверный логин и/или пароль
+          {isError && (
+            <div className={styles.formErrorMessageWrapper}>
+              <span className={styles.errorMessage}>
+                Неверный логин и/или пароль
+              </span>
             </div>
-          ) : null}
+          )}
 
           <div className={styles.inputsContainer}>
             <label className={styles.label} htmlFor='username'>
               Почта
             </label>
+            <span className={styles.inputErrorMessageWrapper}>
+              <span className={styles.errorMessage}>
+                {errors.username?.message}
+              </span>
+            </span>
             <input
               className={styles.input}
               id='username'
               type='text'
-              {...register('username')}
+              {...register('username', { required: true, maxLength: 15 })}
             />
 
             <label className={styles.label} htmlFor='password'>
               Пароль
             </label>
+            <span className={styles.inputErrorMessageWrapper}>
+              <span className={styles.errorMessage}>
+                {errors.password?.message}
+              </span>
+            </span>
             <input
               className={styles.input}
               id='password'
               type='password'
-              {...register('password')}
+              {...register('password', { required: true })}
             />
           </div>
           <div className={styles.controls}>
             <div className={styles.register}>Запросить доступ</div>
             <button className={styles.submit} type='submit'>
-              Submit
+              Войти
             </button>
           </div>
         </form>
